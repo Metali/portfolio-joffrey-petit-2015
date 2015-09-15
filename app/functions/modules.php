b4906eb1f13700aa31e4f6075b6ca56a4fa55404
Ivan Daum <ivan.daum@gmail.com>
2015-09-15 13:10:10 +0200

    fix(media view): remove var_dump

--- app/functions/modules.php
+++ app/functions/modules.php
@@ -4,7 +4,6 @@ function mediaModule($media)
     $view = "";
    
     foreach ($media as $picture) {
-    	 var_dump($picture);die;
         $view .= '
         <a href="' . $picture["preview"][0] . '" class="picture">
             <img width="' . $picture["preview"][2] . '" height="' . $picture["preview"][1] . '" src="' . $picture["preview"][0] . '" />
