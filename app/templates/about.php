948762067b3b677748396768872be0d8fd613cc9
Ivan Daum <ivan.daum@gmail.com>
2016-09-19 22:32:15 +0200

    feat(navbar): finish nav

--- app/templates/about.php
+++ app/templates/about.php
@@ -1,12 +1,12 @@
+<div id="about-navigation" style="height:<?= $anchor_menu_height ?>px">
+    <ul id="navigation-dots">
+        <?= $anchor_dots ?>
+    </ul>
+    <div id="dot-loading"></div>
+</div>
 <div id="about">
     <div id="about-content" class="page-content container">
         <?= $page['content']; ?>
-    </div>
-    <div id="about-navigation" style="height:<?= $anchor_menu_height ?>px">
-        <ul id="navigation-dots">
-            <?= $anchor_dots ?>
-        </ul>
-        <div id="dot-loading"></div>
     </div>
     <div id="about-cover">
         <img src="<?= $page['image']['full']; ?>" />
--- app/templates/partials/navbar.php
+++ app/templates/partials/navbar.php
@@ -8,4 +8,14 @@
         <span>Maquilleur professionnel</span>
     </a>
 </nav>
+
+<div id="nav-full">
+    <ul id="nav-full-menu">
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/galerie/">Galerie</a></li>
+        <li><a href="/a-propos/">À propos</a></li>
+        <li><a href="/contact/">Contact</a></li>
+    </ul>
+</div>
+
 <div id="page-content" class="container">
